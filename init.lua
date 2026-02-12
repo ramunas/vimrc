4b42ee0ca3c77d2e1ccc46c7e62d3268f9ac98c9
Ramūnas Forsberg Gutkovas <ramunasg@gmail.com>
2026-02-12 10:08:30 +0100

    added neovim config

---
+++ init.lua
@@ -0,0 +1,137 @@
+vim.g.mapleader = ','
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.tabstop = 2
+vim.opt.cursorcolumn = false
+vim.opt.ignorecase = false
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.number = false
+vim.opt.relativenumber = false
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.signcolumn = "no"
+vim.opt.splitright = true
+
+
+vim.keymap.set('n', '<leader>n', ':nohlsearch<cr>')
+vim.keymap.set('n', '<leader>m', ':nohlsearch<cr>')
+
+vim.keymap.set('i', '<c-a>', '<c-o><home>')
+vim.keymap.set('i', '<c-e>', '<c-o><end>')
+vim.keymap.set('i', '<c-f>', '<right>')
+vim.keymap.set('i', '<c-b>', '<left>')
+vim.keymap.set('i', '<c-n>', '<down>')
+vim.keymap.set('i', '<c-d>', '<c-o>x')
+
+vim.keymap.set('n', '<c-j>', '<c-w>-')
+vim.keymap.set('n', '<c-k>', '<c-w>+')
+vim.keymap.set('n', '<c-h>', '<c-w><')
+vim.keymap.set('n', '<c-l>', '<c-w>>')
+
+vim.keymap.set('n', 'Y', 'y$')
+vim.keymap.set('v', 'gy', '"+y')
+vim.keymap.set('n', 'gp', '"+p')
+vim.keymap.set('n', 'gP', '"+P')
+
+vim.keymap.set('c', '<c-p>', '<up>')
+vim.keymap.set('c', '<c-n>', '<down>')
+
+vim.keymap.set('n', '<C-t><C-h>', ':tabprevious<CR>')
+vim.keymap.set('n', '<C-t><C-l>', ':tabnext<CR>')
+vim.keymap.set('n', '<C-t><C-t>', ':tabnext<CR>')
+
+vim.keymap.set('t', '<c-w>', '<c-\\><c-n><c-w>')
+vim.cmd([[autocmd TermOpen * startinsert]])
+
+vim.cmd([[
+autocmd FileType cpp,c setlocal formatprg=clang-format
+autocmd FileType javascript setlocal formatprg=prettier\ --parser=babel\ --tab-width\ 4
+]])
+
+vim.lsp.enable('clangd')
+vim.cmd [[map <leader>h :LspClangdSwitchSourceHeader<cr>]]
+
+vim.lsp.enable('ts_ls') -- javascript, jsx, etc.
+vim.lsp.enable('cmake')
+vim.lsp.enable('bashls')
+vim.lsp.enable('pyright') -- python
+
+-- To install plug.vim
+-- curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+--       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'tpope/vim-fugitive'
+Plug 'tpope/vim-eunuch'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'itchyny/vim-cursorword'
+Plug 'tomtom/tcomment_vim'
+Plug 'nvim-tree/nvim-tree.lua'
+Plug 'junegunn/fzf'
+Plug 'junegunn/fzf.vim'
+Plug 'sainnhe/everforest'
+Plug 'neovim/nvim-lspconfig'
+
+vim.call('plug#end')
+
+vim.opt.background = 'light'
+vim.cmd 'colorscheme everforest'
+
+vim.cmd([[map <leader>cc gcc]])
+
+pcall(function()
+	require("telescope").setup()
+
+	local builtin = require('telescope.builtin')
+	vim.keymap.set('n', '<leader>b', builtin.buffers)
+	vim.keymap.set('n', '<leader>f', builtin.find_files)
+	vim.keymap.set('n', '<leader>g', builtin.live_grep)
+end)
+
+pcall(function()
+	require("nvim-tree").setup({
+		respect_buf_cwd = true,
+		renderer = {
+			icons = {
+				show = {
+					file = false,
+					folder = false,
+					folder_arrow = true,
+				},
+				glyphs = {
+					folder = {
+						arrow_closed = '>',
+						arrow_open = 'v'
+					}
+				}
+			}
+		}
+	})
+
+	vim.keymap.set('n', '<leader>t', ':NvimTreeOpen<cr>')
+end)
+
+
+vim.api.nvim_create_user_command('To', function()
+	local bufnr = vim.api.nvim_get_current_buf()
+	vim.cmd.tabnew()
+	vim.cmd(string.format("buffer %d", bufnr))
+end, {})
+vim.api.nvim_create_user_command('TO', ':To', {})
+
+vim.api.nvim_create_user_command('Bdelete', function()
+	local bufnr = vim.api.nvim_get_current_buf()
+	vim.cmd('bnext')
+	vim.api.nvim_buf_delete(bufnr, {})
+end, {})
+
